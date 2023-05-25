981f60a1b2692bed92914424bcea3bb3fbfc6890
João Ryan Santos <sjoaoryan@gmail.com>
2023-05-25 10:20:30 -0300

    feat: tela perfil

--- frontend/src/pages/Perfil/index.tsx
+++ frontend/src/pages/Perfil/index.tsx
@@ -8,7 +8,7 @@ import AnimateWhenVisible from '../../components/AnimateWhenVisible';
 import { MdGroupAdd } from 'react-icons/md';
 import { MdAccountCircle } from 'react-icons/md'
 import { RiContactsFill } from 'react-icons/ri'
-import { RiLogoutBoxRLine } from 'react-icons/ri'
+import { FaArrowLeft } from 'react-icons/fa'
 import TeamCard from '../../components/Equip';
 import Header from '../../components/Hader';
 import image from '../../assets/image/cat2.jpg';
@@ -30,14 +30,48 @@ export function Profile(): JSX.Element {
        </Styled.backgroundGif>
        */}
         <Header />
+        <Styled.RegressDiv>
+          <Styled.Regress onClick={() => navigate(`/home`)}>
+            <FaArrowLeft />
+            voltar
+          </Styled.Regress>
+        </Styled.RegressDiv>
+
         <Styled.BodyDiv>
           <Styled.Image>
             <img src={image} />
           </Styled.Image>
 
-          <img src={image} />
+          <Styled.InfoDiv>
+            <Styled.Title>Minhas informações</Styled.Title>
+            <Styled.Info>
+              <div>
+                <Styled.Subtitle>Nome</Styled.Subtitle>
+                <Styled.Text>Joao Ryan dos Santos</Styled.Text>
+              </div>
+              <div>
+                <Styled.Subtitle>CPF</Styled.Subtitle>
+                <Styled.Text>Joao Ryan dos Santos</Styled.Text>
+              </div>
+              <div>
+                <Styled.Subtitle>Endereço</Styled.Subtitle>
+                <Styled.Text>Joao Ryan dos Santos</Styled.Text>
+              </div>
+              <div>
+                <Styled.Subtitle>Telefone</Styled.Subtitle>
+                <Styled.Text>Joao Ryan dos Santos</Styled.Text>
+              </div>
+            </Styled.Info>
+            <Styled.DivButton>
+              <Styled.Button type="submit">
+                deletar
+              </Styled.Button>
+              <Styled.Button type="submit" >
+                editar
+              </Styled.Button>
+            </Styled.DivButton>
 
-
+          </Styled.InfoDiv>
         </Styled.BodyDiv>
       </Styled.Container>
     </>
